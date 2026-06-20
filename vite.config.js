fd95d906ab10fb65c36bbdef29ade01772fa758f
sujacse11 <sujacse11@gmail.com>
2026-06-20 09:00:49 +0530

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // ADD THIS EXACT LINE (Put your actual GitHub repository name between the slashes)
-  base: '/cake/', 
-  plugins: [react()],
-})
